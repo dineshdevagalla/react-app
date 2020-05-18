6bb58a669df60b3bd47f3bd1337bb17c8c84789e
dineshdevagalla <dinesh.nanda00@gmail.com>
2020-05-18 16:39:32 +0000

    allll

--- src/Common/stores/index.js
+++ src/Common/stores/index.js
@@ -2,20 +2,18 @@ import AuthAPI from "../../Authentication/services/AuthService/AuthAPI";
 import AuthStore from "../../Authentication/stores/AuthStore";
 import ProductsStore from '../../E-commerceDashboard/stores/ProductsStore'
 import ProductsService from '../../E-commerceDashboard/services/ProductsService/ProductsAPI'
-import  CartStore from '../../E-commerceCart/stores/CartStore'
+import CartStore from '../../E-commerceCart/stores/CartStore'
 //import {getAccessToken} from '../../Authentication/utils/StorageUtils.js'
 
 
 
-const authAPI=new AuthAPI()
-const authStore=new AuthStore(authAPI)
+const authAPI = new AuthAPI()
+const authStore = new AuthStore(authAPI)
 
-const productsService=new ProductsService()
-const productsStore=new ProductsStore(productsService)
-
-
-const cartStore=new CartStore(productsStore)
-
-export default {authStore,productsStore,cartStore}
+const productsService = new ProductsService()
+const productsStore = new ProductsStore(productsService)
 
 
+const cartStore = new CartStore(productsStore)
+
+export default { authStore, productsStore, cartStore }
